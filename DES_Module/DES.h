14e9e394bad6ed38e80fe6d25119f744c736a42f
EddieWyx <yixiangw@buffalo.edu>
2015-10-28 19:40:35 -0400

    DES_Module

---
+++ DES_Module/DES.h
@@ -0,0 +1,45 @@
+#ifndef DES_H
+#define DES_H
+
+class Des
+{
+
+	private:
+    	int keyi[16][48],
+        	total[64],
+        	left[32],
+        	right[32],
+        	ck[28],
+        	dk[28],
+        	expansion[48],
+        	z[48],
+        	xor1[48],
+        	sub[32],
+        	p[32],
+        	xor2[32],
+        	temp[64],
+        	pc1[56],
+        	ip[64],
+        	inv[8][8],
+        	key[64];
+ 		char final[1000];
+	public:
+    	Des();
+    	void IP();
+    	void PermChoice1();
+    	void PermChoice2();
+    	void Expansion();
+    	void inverse();
+    	void xor_two();
+    	void xor_oneE(int);
+    	void xor_oneD(int);
+    	void substitution();
+    	void permutation();
+    	void keygen();
+    	void rkeygen();
+    	char * Encrypt(char *);
+    	char * Decrypt(char *);
+};
+
+#endif
+
