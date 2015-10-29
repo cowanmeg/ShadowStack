72343c05e906cfdb703f5e30bd5c1803beec510c
EddieWyx <yixiangw@buffalo.edu>
2015-10-28 20:40:35 -0400

    DES

---
+++ DES_Module/test.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+# include <stdio.h>
+# include <fstream>
+# include <string.h>
+# include <stdlib.h>
+
+# include "DES.h"
+
+using namespace std;
+
+int main()
+{
+    Des d1,d2;
+    char *str  = new char[1000];
+    char *str1 = new char[1000];
+    //strcpy(str,"PHOENIX it & ece solutions.");
+    cout<<"Enter a string : ";
+    cin >> str;
+    str1=d1.Encrypt(str);
+    cout<<"\ni/p Text: "<<str<<endl;
+    cout<<"\nCypher  : "<<str1<<endl;
+    //  ofstream fout("out2_fil.txt"); fout<<str1; fout.close();
+    cout<<"\no/p Text: "<<d2.Decrypt(str1)<<endl;
+    return 0;
+}
