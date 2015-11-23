223c8c119db5050aecc678eeb67d571d76566b90
EddieWyx <yixiangw@umich.edu>
2015-11-22 19:44:07 -0500

    the final encryption

--- encrypt/main.cpp
+++ encrypt/main.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 int main()
 {
-    uint8_t buffer[] = {5,7,3,4,1,9,3};
-    uint8_t key_length = 7;
-    RC4 rc(buffer,key_length);
-
+    // uint8_t buffer[] = {5,7,3,4,1,9,3};
+    // uint8_t key_length = 7;
+    
+    RC4 rc;
     uint8_t in = 99;
     cout << "value is " << unsigned(in) << endl;
     uint8_t out = rc.encryptByte(in);
